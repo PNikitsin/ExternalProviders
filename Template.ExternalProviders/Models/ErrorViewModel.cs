a1b1c781cc3668b610cb93662ed76230c0ca3356
PNikitsin <pnikitinbox@gmail.com>
2022-09-01 19:41:50 +0300

    Add project files.

---
+++ Template.ExternalProviders/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Template.ExternalProviders.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
