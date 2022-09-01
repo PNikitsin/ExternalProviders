11ac84d6d8b4b7d2713ff523a0276a3df7fa3470
PNikitsin <pnikitinbox@gmail.com>
2022-09-01 20:05:03 +0300

    Add google authentication

--- Template.ExternalProviders/Program.cs
+++ Template.ExternalProviders/Program.cs
@@ -15,6 +15,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+        options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+    });
+
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
