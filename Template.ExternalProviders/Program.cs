52561b99f6efb03758dcaba94a4258eaa6bdbb35
PNikitsin <pnikitinbox@gmail.com>
2022-09-02 08:38:42 +0300

    Add facebook authentication

--- Template.ExternalProviders/Program.cs
+++ Template.ExternalProviders/Program.cs
@@ -20,6 +20,11 @@ builder.Services.AddAuthentication()
     {
         options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
         options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+    })
+    .AddFacebook(options =>
+    {
+        options.AppId = builder.Configuration["Authentication:Facebook:AppId"];
+        options.AppSecret = builder.Configuration["Authentication:Facebook:AppSecret"];
     });
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
